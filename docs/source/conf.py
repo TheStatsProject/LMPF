22ae309f20c3305b2b54f9ef11fd019dbe629cc2
TheStatsProject <digroom@tutamail.com>
2025-05-13 14:54:42 -0600

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'The Truth Project'
+copyright = '2025, TheOs'
+author = 'OS'
 
 release = '0.1'
 version = '0.1.0'
