f4f38fe2c577550949cec0d21995ba87ba704265
TheStatsProject <digroom@tutamail.com>
2025-07-21 21:51:42 -0600

    Update models.py

--- app/models.py
+++ app/models.py
@@ -6,11 +6,20 @@ def get_user(username):
 def create_user(username, password_hash):
     from app.database import users_col
     users_col.insert_one({"username": username, "password": password_hash})
-
+    
 def create_subscription(username, plan, payment_status):
     from app.database import subs_col
-    subs_col.insert_one({"username": username, "plan": plan, "payment_status": payment_status})
+    subs_col.insert_one({
+        "username": username,
+        "plan": plan,
+        "payment_status": payment_status
+    })
 
 def record_payment(username, method, details, status):
     from app.database import payments_col
-    payments_col.insert_one({"username": username, "method": method, "details": details, "status": status})
+    payments_col.insert_one({
+        "username": username,
+        "method": method,
+        "details": details,
+        "status": status
+    })
