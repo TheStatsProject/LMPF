17cbfc2cef817a80352136179659d949afef5d92
TheStatsProject <digroom@tutamail.com>
2025-07-21 23:00:47 -0600

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,8 +1,16 @@
-from pymongo import MongoClient
-from app.config import MONGO_URL, DB_NAME
 
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
+MONGO_URL = os.getenv("MONGO_URL")
 client = MongoClient(MONGO_URL)
-db = client[DB_NAME]
+db = client["LMBD"]  # Change to your database name
+
+# Example collection
+users_col = db["users"]
 users_col = db["users"]
 subs_col = db["subscriptions"]
 payments_col = db["payments"]
+
