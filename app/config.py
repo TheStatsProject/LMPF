b19e4da006d25e5758372379841e21b8e77ce13b
TheStatsProject <digroom@tutamail.com>
2025-07-30 22:49:44 -0600

    Update config.py

--- app/config.py
+++ app/config.py
@@ -4,6 +4,9 @@ import os
 # Automatically load .env file at startup
 load_dotenv()
 
+MONGODB_URI = os.getenv("MONGODB_URI", "mongodb://localhost:27017")
+DATABASE_NAME = os.getenv("DATABASE_NAME", "lmpf_db")
+
 # Config values with defaults and error handling
 SECRET_KEY = os.environ.get("SESSION_SECRET_KEY")
 if not SECRET_KEY:
