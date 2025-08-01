2d15ff1501963ac3d67f2c2ab5f1324faca07c3e
TheStatsProject <digroom@tutamail.com>
2025-08-01 14:13:50 -0600

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,3 @@
 # This file marks the 'app' directory as a Python package.
 # You can use it to initialize package-wide settings if needed.
+"""LMPF: Labor Market and Population Forecasting Toolkit package."""
