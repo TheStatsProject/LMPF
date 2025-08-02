2d56333186b0727f8284cc8267e082e0d0de4dbe
TheStatsProject <digroom@tutamail.com>
2025-08-01 22:53:11 -0600

    Update __init__.py

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,5 +1,4 @@
 # This file marks the 'routes' directory as a Python package.
 # You can use it to import route modules for easy inclusion in main.py, e.g.:
 # from . import auth, subscription, payment
-"""LMPF: Labor Market and Population Forecasting Toolkit package."""
 
